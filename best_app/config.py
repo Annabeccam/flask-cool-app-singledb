82ce4739142d01328a355b2204395bba01f3b6a0
Sheetal <sheetal@Sheetals-MacBook-Pro.local>
2024-11-22 12:37:23 +0200

    wip

--- best_app/config.py
+++ best_app/config.py
@@ -1,3 +1,6 @@
+import os
+
 class DbConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://admin_user1:12345@127.0.0.1:5432/single_db"
+    #SQLALCHEMY_DATABASE_URI = "postgresql://admin_user1:12345@127.0.0.1:5432/single_db"
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
